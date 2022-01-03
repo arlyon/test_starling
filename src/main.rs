f2b11fb8a4c51dcdbc4d750ac116f3e51015dbd7
Richard <richlyon@mac.com>
2022-01-03 19:55:46 +0000

    Add command line interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::{Parser, Subcommand};
 mod client;
 use crate::client::StarlingAccount;
 use serde::{Deserialize, Serialize};
@@ -8,16 +9,49 @@ struct Tokens {
     business: String,
 }
 
+/// CLI arguments
+#[derive(Parser, Debug, Clone)]
+#[clap(about, version, author)]
+struct Args {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+/// CLI Commands
+#[derive(Clone, Debug, Subcommand)]
+enum Command {
+    /// Account balances
+    Balances,
+
+    /// Account Transactions
+    Transactions {
+        //// Days to get
+        #[clap(short, long, default_value_t = 7)]
+        days: i64,
+    },
+}
+
 #[tokio::main]
 async fn main() {
+    let args = Args::parse();
+
     // Get tokens
     let f = std::fs::File::open(String::from("tokens.yml")).expect("Couldn't open tokens_file");
     let tokens: Tokens = serde_yaml::from_reader(f).expect("Could not deserialise yaml");
 
     // Get accounts
     let account = StarlingAccount::new(tokens.personal).await;
-    
-    for transaction in account.transactions_since(chrono::Duration::days(7)).await {
-        println!("{}", transaction.to_string());
+
+    match args.command {
+        Command::Balances => todo!(),
+
+        Command::Transactions { days } => {
+            for transaction in account
+                .transactions_since(chrono::Duration::days(days))
+                .await
+            {
+                println!("{}", transaction.to_string());
+            }
+        }
     }
 }
